7739a8a0d094222b9c9366806afe73253dae985f
saqlainmuhammadd <miansaqlainqureshi@gmail.com>
2024-07-02 02:38:28 +0100

    updated

--- problem15.py
+++ problem15.py
@@ -2,3 +2,9 @@
 # table : “a piece of furniture”, “list of facts & figures”
 #  cat : “a small animal”
 
+dict = {
+    table : (
+        "A piece of forniture",
+        'list of facts & figures'
+    )
+}
